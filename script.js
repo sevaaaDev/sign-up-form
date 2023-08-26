a25391f908097cc9c3da3bbfa0c285d08f8919ad
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-08-26 16:33:16 +0700

    add form validation

---
+++ script.js
@@ -0,0 +1,122 @@
+const form = document.querySelector('form')
+const firstName = document.getElementById('firstName')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const confirmPw = document.getElementById('confirmPw')
+const firstNameError = document.querySelector('#firstName + .error')
+const emailError = document.querySelector('#email + .error')
+const passwordError = document.querySelector('#password + .error')
+const confirmPwError = document.querySelector('#confirmPw + .error')
+
+let removeError = {
+  password: () => {
+    passwordError.innerText = ``
+  },
+  confirmPw: () => {
+    confirmPwError.innerText = ``
+  },
+  email: () => {
+    emailError.innerText = ``
+  },
+  firstName: () => {
+    firstNameError.innerText = ``
+  },
+}
+
+let error = {
+  password: (errorType) => {
+    passwordError.innerText = `* Password ${errorType}`
+  },
+  confirmPw: (errorType) => {
+    confirmPwError.innerText = `* Confirm password ${errorType}`
+  },
+  email: (errorType) => {
+    emailError.innerText = `* Email ${errorType}`
+  },
+  firstName: (errorType) => {
+    firstNameError.innerText = `* First Name ${errorType}`
+  }
+}
+
+firstName.addEventListener('input', () => {
+  if (firstName.validity.valid) {
+    removeError[firstName.id]()
+  } else {
+    showError(firstName)
+  }
+})
+
+email.addEventListener('input', () => {
+  if (email.validity.valid) {
+    removeError[email.id]()
+  } else {
+    showError(email)
+  }
+})
+
+password.addEventListener('input', () => {
+  if (password.validity.valid) {
+    removeError[password.id]()
+  } else if (isMatched(password.value, confirmPw.value)) {
+    removeError[confirmPw.id]()
+  } 
+  
+  if (!password.validity.valid && !isMatched(password.value, confirmPw.value)) {
+    error['confirmPw']('do not match')
+  } else if (!password.validity.valid) {
+    showError(password)
+  }
+
+})
+
+confirmPw.addEventListener('input', () => {
+  if (isMatched(password.value, confirmPw.value)) {
+    removeError[confirmPw.id]()
+  } else {
+    error['confirmPw']('do not match')
+  }
+})
+
+form.addEventListener('submit', (e) => {
+  if (isValid() === false) {
+    showError(password, confirmPw, email, firstName)
+    e.preventDefault()
+  }
+})
+
+function isMatched(pw, confirmPw) {
+  console.log({ pw })
+  console.log({ confirmPw })
+  if (pw !== confirmPw && confirmPw  !== '') {
+    console.log('no match')
+    valid = false
+    return false
+  } 
+  return true
+}
+
+function showError(...element) {
+  for (const input of element) {   
+    if (input.validity.tooShort) {
+      error[input.id]('must be 12 char or more')
+      console.log('short')
+    } else if (input.validity.valueMissing) {
+      error[input.id]('is REQUIRED')
+      console.log('required')
+    } else if (input.validity.typeMismatch) {
+      error[input.id]('should be in the right format')
+    }
+  }
+}
+
+function isValid() {
+  if (password.validity.valid &&
+      email.validity.valid &&
+      firstName.validity.valid &&
+      confirmPw.validity.valid) 
+    {
+      return true
+    }
+  return false
+}
+
